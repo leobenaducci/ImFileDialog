03f1e2ac45da000813eacb5dead4e18f40e66193
leobenaducci <leobenaducci@hotmail.com>
2023-08-01 14:11:13 -0400

    removed stb_implementation definition fixed imgui headers directory

--- ImFileDialog.cpp
+++ ImFileDialog.cpp
@@ -7,10 +7,10 @@
 #include <algorithm>
 #include <sys/stat.h>
 #define IMGUI_DEFINE_MATH_OPERATORS
-#include <imgui/imgui.h>
-#include <imgui/imgui_internal.h>
+#include "imgui.h"
+#include "imgui_internal.h"
 
-#define STB_IMAGE_IMPLEMENTATION
+//#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #ifdef _WIN32
